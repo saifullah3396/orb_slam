85c79c6f28cc8d60b1f73c0e0150e86e0872e05b
saifullah3396 <Saifullah3396@gmail.com>
2020-02-04 16:58:44 +0500

    New camera properties...

--- include/orb_slam/geometry/camera.h
+++ include/orb_slam/geometry/camera.h
@@ -44,20 +44,45 @@ public:
     virtual ~Camera();
 
     /**
+     * @brief undistortPoints Performs undistortion operation on array of key
+     *     points
+     * @param key_points: Input distorted key points
+     * @param undist_key_points: Output undistorted key points
+     * @param undist_intrinsic_matrix: The output undistorted intrinsic matrix
+     */
+    void undistortPoints(
+        std::vector<cv::KeyPoint>& key_points,
+        std::vector<cv::KeyPoint>& undist_key_points,
+        cv::Mat& undist_intrinsic_matrix);
+
+    /**
+     * @brief computeImageBounds Computes the image bounds while taking into
+     *     account the distortion in the image
+     */
+    void computeImageBounds();
+
+    /**
      * Getters
      */
-    uint16_t fps() { return fps_; }
-    uint32_t width() { return width_; }
-    uint32_t height() { return height_; }
-    T fovX() { return fov_x_; }
-    T fovY() { return fov_y_; }
-    T focalX() { return focal_x_; }
-    T focalY() { return focal_y_; }
-    T invFocalX() { return focal_x_inv_; }
-    T invFocalY() { return focal_y_inv_; }
-    T centerX() { return center_x_; }
-    T centerY() { return center_y_; }
+    const int& fps() { return fps_; }
+    const int& width() { return width_; }
+    const int& height() { return height_; }
+    const int& undistWidth() { return undist_width_; }
+    const int& undistHeight() { return undist_height_; }
+    const int& minX() const { return min_x_; }
+    const int& minY() const { return min_y_; }
+    const int& maxX() const { return max_x_; }
+    const int& maxY() const { return max_y_; }
+    const T& fovX() const { return fov_x_; }
+    const T& fovY() const { return fov_y_; }
+    const T& focalX() const { return focal_x_; }
+    const T& focalY() const { return focal_y_; }
+    const T& invFocalX() const { return focal_x_inv_; }
+    const T& invFocalY() const { return focal_y_inv_; }
+    const T& center() const { return center_x_; }
+    const T& center() const { return center_y_; }
     virtual cv::Mat image() = 0;
+    virtual CameraType type() = 0;
     virtual cv::Mat imageL() = 0;
     virtual cv::Mat imageR() = 0;
     virtual cv::Mat imageDepth() = 0;
@@ -77,21 +102,17 @@ private:
      */
     void updateIntrinsicMatrix();
 
-    /**
-     * @brief undistortPoints Performs undistortion operation on array of key
-     *     points
-     * @param key_points: Input distorted key points
-     * @param undist_key_points: Output undistorted key points
-     * @param undist_intrinsic_matrix: The output undistorted intrinsic matrix
-     */
-    void undistortPoints(
-        std::vector<cv::KeyPoint>& key_points,
-        std::vector<cv::KeyPoint>& undist_key_points,
-        cv::Mat& undist_intrinsic_matrix);
-
-    uint16_t fps_ = {30}; //! Frames per second for the video
-    uint32_t width_ = {0}; //! Image width
-    uint32_t height_ = {0}; //! Image height
+    CameraType type; //! Camera type
+    int  fps_ = {30}; //! Frames per second for the video
+    int  width_ = {0}; //! Image width
+    int  height_ = {0}; //! Image height
+    int undist_width_ = {0}; //! Width after distortion is removed
+    int undist_height_ = {0}; //! Height after distortion is removed
+    cv::Mat undist_bounds = {cv::Mat_<T>(4, 2, CV_32F)};
+    T min_x_; //! Min x after removed distortion
+    T max_x_; //! Max x after removed distortion
+    T min_y_; //! Min y after removed distortion
+    T max_y_; //! Max y after removed distortion
     T fov_x_ = {0}; //! Camera field of view X
     T fov_y_ = {0}; //! Camera field of view Y
     T focal_x_ = {0}; //! Camera focal length X
@@ -132,6 +153,7 @@ public:
     ~MonoCamera();
 
     cv::Mat image() { return image_; }
+    CameraType type() { return CameraType::MONO; }
     cv::Mat imageL() {
         throw std::runtime_error(
             "imageL() is undefined for monocular camera.");
