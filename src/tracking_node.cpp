2366f0b057b8a686d8c6807796870da3c880b6b9
saifullah3396 <Saifullah3396@gmail.com>
2020-02-07 10:33:43 +0500

    Fixed the definition of tracker node. Since tracker has a unique_ptr inside. It cannot be copy initialized.

--- src/tracking_node.cpp
+++ src/tracking_node.cpp
@@ -4,21 +4,21 @@
  */
 
 #include <ros/ros.h>
-#include "orb_slam/tracker.h"
+#include <memory>
+#include <orb_slam/tracker.h>
 
-namespace orb_slam {
+using namespace orb_slam;
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "tracking_node");
     ros::NodeHandle nh;
-    Tracker(nh);
+    auto tracker =
+        std::unique_ptr<Tracker>(new Tracker(nh));
     auto rate = ros::Rate(1000);
     while (ros::ok()) {
         ros::spinOnce();
         rate.sleep();
     }
     return 0;
-}
-
 }
