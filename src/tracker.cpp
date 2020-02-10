42af0e99c856f20a28eba3228be6c60ce9dadcc7
saifullah3396 <Saifullah3396@gmail.com>
2020-02-10 09:38:01 +0500

    Updated according to changes in camera.

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -18,7 +18,9 @@ namespace orb_slam
 Tracker::Tracker(const ros::NodeHandle& nh) : nh_(nh)
 {
     // initialize the camera
-    camera_ = geometry::CameraPtr<float>(new geometry::MonoCamera<float>(nh_));
+    camera_ = geometry::CameraPtr<float>(new geometry::MonoCamera<float>());
+    camera_->readParams(nh_);
+    camera_->setup();
     orb_extractor_ =
         geometry::ORBExtractorPtr(new geometry::ORBExtractor(nh_));
     orb_matcher_ =
