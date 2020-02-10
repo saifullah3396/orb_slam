61bea434a051c4b02d363b1c7d91841ba96f7007
saifullah3396 <Saifullah3396@gmail.com>
2020-02-10 15:59:55 +0500

    Added debugging logger level.

--- src/tracker_node.cpp
+++ src/tracker_node.cpp
@@ -6,13 +6,19 @@
 #include <ros/ros.h>
 #include <memory>
 #include <orb_slam/tracker.h>
-
+#include <ros/console.h>
 using namespace orb_slam;
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "tracking_node");
     ros::NodeHandle nh;
+
+    if(ros::console::set_logger_level(
+            ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+
     auto tracker =
         std::unique_ptr<Tracker>(new Tracker(nh));
     ros::spin();
