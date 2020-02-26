07281adef21d9419f63a13fab3497b22033b12fb
saifullah3396 <Saifullah3396@gmail.com>
2020-02-26 12:02:53 +0500

    Added a rgbd for tracking rgbd images.

---
+++ include/orb_slam/rgbd_tracker.h
@@ -0,0 +1,41 @@
+/**
+ * Declares the RGBDTracker class.
+ */
+
+#pragma once
+
+#include <ros/ros.h>
+#include <orb_slam/tracker.h>
+
+namespace orb_slam
+{
+
+class KeyFrame;
+using KeyFramePtr = std::shared_ptr<KeyFrame>;
+
+class RGBDTracker : public Tracker {
+public:
+    /**
+     * Constructor
+     * @param nh: ROS node handle
+     */
+    RGBDTracker(const ros::NodeHandle& nh, const int& camera_type);
+
+    /**
+     * Destructor
+     */
+    ~RGBDTracker();
+
+    void update();
+    void initializeTracking();
+
+private:
+    cv_bridge::CvImageConstPtr last_image_;
+    cv_bridge::CvImageConstPtr last_depth_;
+    KeyFramePtr last_key_frame_;
+
+    float depth_map_scale_; // for some datasets depth maps are scaled
+};
+using RGBDTrackerPtr = std::shared_ptr<RGBDTracker>;
+
+} // namespace orb_slam
