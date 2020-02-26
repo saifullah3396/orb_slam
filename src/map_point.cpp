c5064df052651faa79ae48c82213696c70a6fd74
saifullah3396 <Saifullah3396@gmail.com>
2020-02-26 12:22:27 +0500

    Finds the best descriptor by matching the same point in all key frames it is observed. The best descriptor is found as the median of all the descriptors as done in original orb slam repository.

--- src/map_point.cpp
+++ src/map_point.cpp
@@ -86,4 +86,63 @@ void MapPoint::removeFromMap() {
     map_->removeMapPoint(shared_from_this());
 }
 
+void MapPoint::computeBestDescriptor()
+{
+    // Retrieve all observed descriptors
+    std::vector<cv::Mat> descriptors;
+    std::map<KeyFramePtr, size_t> observations;
+    { // shared
+        std::unique_lock<std::mutex> observations_lock(observations_mutex_);
+        if (bad_point_ || observations_.empty()) // return if bad point since it has to be deleted
+            return;
+        observations = observations_; // get a local copy of observations
+    }
+
+    descriptors.reserve(observations.size());
+    // get descriptors of matching points
+    for (auto it = observations.begin(); it != observations.end(); ++it) {
+        auto key_frame = it->first;
+        if (!key_frame->isBad()) { // if key frame is not to be deleted
+            descriptors.push_back(key_frame->descriptor(it->second));
+        }
+    }
+
+    if (descriptors.empty())
+        return;
+
+    // compute distances between the descriptors
+    const size_t n = descriptors.size();
+
+    float distances[n][n];
+    for (size_t i = 0; i < n; i++) {
+        distances[i][i] = 0;
+        for (size_t j = i + 1; j < n; j++) {
+            int dist =
+                geometry::descriptorDistance(descriptors[i], descriptors[j]);
+            distances[i][j] = dist;
+            distances[j][i] = dist;
+        }
+    }
+
+    // take the descriptor with least median distance to the rest
+    int best_median = INT_MAX;
+    int best_idx = 0;
+    for (size_t i = 0; i < n; i++) {
+        std::vector<int> dists(distances[i], distances[i] + n);
+        sort(dists.begin(),dists.end());
+        int median = dists[0.5 * (n - 1)]; // find median
+
+        if(median < best_median)
+        {
+            best_median = median;
+            best_idx = i;
+        }
+    }
+
+    { // shared
+        std::unique_lock<std::mutex> observations_lock(observations_mutex_);
+        best_descriptor_ = descriptors[best_idx].clone();
+    }
+}
+
 } // namespace orb_slam
